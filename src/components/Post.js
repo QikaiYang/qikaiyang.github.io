468554f6422c9eaacc8e85733a7bde6cfeb98529
QikaiYang <qikaiy2@illinois.edu>
2021-11-07 14:17:13 -0600

    index.html

--- src/components/Post.js
+++ src/components/Post.js
@@ -29,10 +29,10 @@ export default function Post() {
             <section className="container mx-auto">
                 <h1 className="text-5xl flex justify-center font-bold">
                     Research
-            </h1>
+                </h1>
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">
                     Welcome to my page of research!
-            </h2>
+                </h2>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {postData &&
                         postData.map((post, index) => (
